fixed moving average transformation

src/napari_pystackreg/_widget.py
@@ -350,6 +350,9 @@ class PystackregWidget(QWidget):
         n_frames = self.n_frames.value()
         reference = self.reference.currentData()
 
+        if reference not in self.REFERENCES:
+            raise ValueError(f'Unknown reference "{reference}"')
+
         image = self.image.currentData()
 
         self.pbar.setMaximum(image.shape[0] - 1)
@@ -370,48 +373,50 @@ class PystackregWidget(QWidget):
             axis = 0
 
             if action in ["register", "register_transform"]:
-
+                image_reg = image
                 idx_start = 1
 
                 if moving_average > 1:
                     idx_start = 0
-                    size = [0] * len(image.shape)
+                    size = [0] * len(image_reg.shape)
                     size[axis] = moving_average
-                    image = running_mean(image, moving_average, axis=axis)
+                    image_reg = running_mean(
+                        image_reg, moving_average, axis=axis
+                    )
 
                 tmatdim = 4 if transformation == "bilinear" else 3
 
                 tmats = np.repeat(
                     np.identity(tmatdim).reshape((1, tmatdim, tmatdim)),
-                    image.shape[axis],
+                    image_reg.shape[axis],
                     axis=0,
                 ).astype(np.double)
 
                 if reference == "first":
                     ref = np.mean(
-                        image.take(range(n_frames), axis=axis), axis=axis
+                        image_reg.take(range(n_frames), axis=axis), axis=axis
                     )
                 elif reference == "mean":
-                    ref = image.mean(axis=0)
+                    ref = image_reg.mean(axis=0)
                     idx_start = 0
                 elif reference == "previous":
                     pass
-                else:
+                else:  # pragma: no cover - can't be reached due to check above
                     raise ValueError(f'Unknown reference "{reference}"')
 
                 self.pbar_label.setText("Registering...")
 
-                iterable = range(idx_start, image.shape[axis])
+                iterable = range(idx_start, image_reg.shape[axis])
 
                 for i in iterable:
-                    slc = [slice(None)] * len(image.shape)
+                    slc = [slice(None)] * len(image_reg.shape)
                     slc[axis] = i
 
                     if reference == "previous":
-                        ref = image.take(i - 1, axis=axis)
+                        ref = image_reg.take(i - 1, axis=axis)
 
                     tmats[i, :, :] = sr.register(
-                        ref, simple_slice(image, i, axis)
+                        ref, simple_slice(image_reg, i, axis)
                     )
 
                     if reference == "previous" and i > 0: